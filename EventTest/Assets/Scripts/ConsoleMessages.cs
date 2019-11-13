40d2085e2351c39ef1c04f41319fd32edb717ffc
PmaiWoW <PmaiWoW@Gmail.com>
2019-11-13 17:32:10 +0000

    Add exs 5 and 6 completed

---
+++ EventTest/Assets/Scripts/ConsoleMessages.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleMessages : MonoBehaviour
+{
+    // Reference to the event master
+    private EventMaster em;
+
+    // Awake is called one time at the beginning
+    private void Awake()
+    {
+        // Get reference to the event master
+        em = GetComponent<EventMaster>();
+    }
+
+    private void OnEnable()
+    {
+        em.KeyPress += ShowConsoleMessage;
+    }
+
+    // Use this to remove event listeners
+    private void OnDisable()
+    {
+        em.KeyPress -= ShowConsoleMessage;
+    }
+
+    private void ShowConsoleMessage(object sender, KeyEventArgs key)
+    {
+        switch (key.KeyPressed)
+        {
+            case 'W':
+                Debug.Log("W was pressed!");
+                break;
+            case 'S':
+                Debug.Log("S was pressed!");
+                break;
+            case 'A':
+                Debug.Log("A was pressed!");
+                break;
+            case 'D':
+                Debug.Log("D was pressed!");
+                break;
+        }
+    }
+}
---
+++ EventTest/Assets/Scripts/KeyEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class KeyEventArgs : EventArgs
+{
+    public char KeyPressed { get; }
+    public KeyEventArgs(char keyPressed)
+    {
+        KeyPressed = keyPressed;
+    }
+}
